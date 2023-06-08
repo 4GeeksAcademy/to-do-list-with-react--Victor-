06fc3cfe335a82786f9498828ffde796b29d9356
VictorMagaCBT <126924738+VictorMagaCBT@users.noreply.github.com>
2023-06-08 22:19:08 +0000

    complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,54 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [task, setTask] = useState ("")
+	const [todoList, setTodoList] = useState([])
+
+	const handleChange = (e) => {
+		setTask(e.target.value);
+	  };
+	
+	  const handleKeyDown = (e) => {
+		if (e.key === 'Enter') {
+			if(task ==="") {alert ("type something")}
+			else setTodoList([...todoList, task])
+			setTask("")}
+	  };
+	
+
+	  const removeElement = (e) => {
+		const newList = todoList.filter((_, i) => i !== parseInt(e.target.id));
+		setTodoList(newList);
+	  };
+
+	  const todoListLenght = todoList.length + " item left"
+		
+		
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="page">
+				<div className="todos d-justify-content-arround">todos
+					<div className="square-list" >												
+						<ul className="list">
+						<input 
+						type="text" 
+						placeholder="Enter task" 
+						value={task} 
+						onChange={handleChange}					
+						onKeyDown={handleKeyDown}/>	
+							{todoList.length !== 0 && todoList.map((todoItem, idx) => {
+								return <li key={idx} >{todoItem}
+        						<button className="delete" id={idx} onClick={removeElement}>X</button>
+      							</li>}
+							)}							
+						</ul>
+					<div className="footer">{todoListLenght} </div>
+					</div>
+				</div>
 		</div>
 	);
 };
